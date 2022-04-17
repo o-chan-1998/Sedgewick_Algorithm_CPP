7172d589c5a34a7102952efdec08ad6d9e883841
o-chan-1998 <root@localhost.localdomain>
2022-04-17 06:49:01 -0400

    Step002_Fix

--- ABC237/D/sample.cpp
+++ ABC237/D/sample.cpp
@@ -11,13 +11,30 @@
 
 using namespace std;
 
-#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define rep(i,n) for (int i = 0; i < (n); i++)
 
 int main(void){
 
-	rep(i, 10){
-		cout << i << endl;
+	int N;
+	string s;
+	cin >> N;
+	cin >> s;
+
+	deque<int> ans = {N};	// キューの初期化
+
+	for(int i=N-1; i>=0; i--){
+		if(s[i]=='L'){
+			ans.push_back(i);	// Lは右に追加
+		}else{
+			ans.push_front(i);	// Rは左に追加
+		}
 	}
-	
+
+	rep(i, N+1){
+		cout << ans[i] << " ";
+	}
+
+	cout << endl;
+
 	return EXIT_SUCCESS;
 }
