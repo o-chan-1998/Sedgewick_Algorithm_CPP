66223f4db36923b9d8ff8248644797082c6cda3a
o-chan-1998 <root@localhost.localdomain>
2022-04-17 06:18:48 -0400

    Step001_初期状態

---
+++ ABC237/D/sample.cpp
@@ -0,0 +1,23 @@
+// https://atcoder.jp/contests/abc190/tasks/abc190_b
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ios>     // std::left, std::right
+#include <iomanip> // std::setw(int), std::setfill(char)
+#include <queue>
+#include <map>
+
+using namespace std;
+
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+int main(void){
+
+	rep(i, 10){
+		cout << i << endl;
+	}
+	
+	return EXIT_SUCCESS;
+}
