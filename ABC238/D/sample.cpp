badd2a4757e1caad0f2f9a22315d6e078ba15789
o-chan-1998 <root@localhost.localdomain>
2022-04-18 06:55:12 -0400

    ABC237_D問題_初期

---
+++ ABC238/D/sample.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ios>     // std::left, std::right
+#include <iomanip> // std::setw(int), std::setfill(char)
+#include <queue>
+#include <map>
+
+using namespace std;
+
+#define rep(i,N) for (int i = 0; i < N; i++)
+#define rep1(i,N) for(int i = 1; i <= N; i++)
+#define drep(i,N) for(int i = N-1; i >= 0; i--)
+#define srep(i,S,T) for (int i = S; i < T; i++)
+
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+using vi = vc<int>; using vvi = vv<int>;
+using vl = vc<long>; using vvl = vv<long>;
+using ll = long long;
+
+int main(void){
+	return EXIT_SUCCESS;
+}
