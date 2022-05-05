94f07445f1a6df55ce2c633b37b901305ab13a05
root <root@localhost.localdomain>
2022-05-05 02:56:06 -0400

    ABC211_D問題_AC版

--- ABC211/D/sample.cpp
+++ ABC211/D/sample.cpp
@@ -40,66 +40,11 @@ class mint
 public:
     long long x;
     constexpr mint(long long x = 0) : x((x % mod + mod) % mod) {}
-    constexpr mint operator-() const
-    {
-        return mint(-x);
-    }
     constexpr mint &operator+=(const mint &a)
     {
         if ((x += a.x) >= mod)
             x -= mod;
         return *this;
-    }
-    constexpr mint &operator-=(const mint &a)
-    {
-        if ((x += mod - a.x) >= mod)
-            x -= mod;
-        return *this;
-    }
-    constexpr mint &operator*=(const mint &a)
-    {
-        (x *= a.x) %= mod;
-        return *this;
-    }
-    constexpr mint operator+(const mint &a) const
-    {
-        mint res(*this);
-        return res += a;
-    }
-    constexpr mint operator-(const mint &a) const
-    {
-        mint res(*this);
-        return res -= a;
-    }
-    constexpr mint operator*(const mint &a) const
-    {
-        mint res(*this);
-        return res *= a;
-    }
-    constexpr mint pow(long long t) const
-    {
-        if (!t)
-            return 1;
-        mint a = pow(t >> 1);
-        a *= a;
-        if (t & 1)
-            a *= *this;
-        return a;
-    }
-
-    // for prime mod
-    constexpr mint inv() const
-    {
-        return pow(mod - 2);
-    }
-    constexpr mint &operator/=(const mint &a)
-    {
-        return (*this) *= a.inv();
-    }
-    constexpr mint operator/(const mint &a) const
-    {
-        mint res(*this);
-        return res /= a;
     }
 };
 
