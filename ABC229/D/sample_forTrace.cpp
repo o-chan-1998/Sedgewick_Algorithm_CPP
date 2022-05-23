dd2eb819c11744215155587bc07600bf87561513
root <root@localhost.localdomain>
2022-05-23 04:32:18 -0400

    ABC229_D問題

--- ABC229/D/sample_forTrace.cpp
+++ ABC229/D/sample_forTrace.cpp
@@ -37,26 +37,51 @@ using vll = vc<ll>;
 using vvll = vv<ll>;
 
 int main() {
+  // ファイル出力
+  const char *fileName = "trace.txt";
+  ofstream ofs(fileName);
+  if (!ofs)
+  {
+    cout << "ファイルが開けませんでした。" << endl;
+    cin.get();
+    return 0;
+  }
+
   string s;
   int k;
   cin >> s >> k;
+ofs << "  cin >> s(" << s << ") >> k(" << k << ");" << endl;
   int n = s.size();
+ofs << "  int n(" << n << ") = s.size();" << endl;
   vector<int> a(n);
   rep(i,n){
     a[i] = s[i]=='.';
   }
 
+ofs << "  rep(i,n){" << endl;
+rep(i,n) ofs << "    a[i](" << a[i] << ") = s[i(" << i << ")](" << s[i] << ")=='.';" << endl;
+ofs << "  }" << endl;
+
   int ans = 0;
   int r = 0, sum = 0;
+ofs << "  rep(l,n(" << n << ")) {" << endl;
   rep(l,n) {
+ofs << "    while (r(" << r << ") < n(" << n << ") && sum+a[r](" << sum+a[r] << ") <= k(" << k << ")) {" << endl;
     while (r < n && sum+a[r] <= k) {
       sum += a[r];
+ofs << "      sum(" << sum << ") += a[r(" << r << ")](" << a[r] << ");" << endl;
       ++r;
     }
+ofs << "    }" << endl;
     ans = max(ans, r-l);
+ofs << "    ans(" << ans << ") = max(ans(" << ans << "), r(" << r << ")-l(" << l << ")(" << r-l << "));" << endl;
     sum -= a[l];
   }
 
   cout << ans << endl;
+ofs << "  cout << ans(" << ans << ") << endl;" << endl;
+ 
+   ofs.close();
+
   return 0;
 }
