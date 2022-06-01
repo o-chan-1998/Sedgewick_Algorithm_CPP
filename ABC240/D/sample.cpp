909e7f90a6518602f525607e3c3ec1f4a12f485b
root <root@localhost.localdomain>
2022-06-01 10:15:43 -0400

    ABC240_D問題

---
+++ ABC240/D/sample.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ios>
+#include <iomanip>
+#include <queue>
+#include <map>
+#include <set>
+#include <climits>
+#include <bitset>
+#include <numeric> // iota関数
+#include <fstream>
+
+using namespace std;
+
+#define rep(i, H) for (int i = 0; i < H; i++)
+#define rep1(i, H) for (int i = 1; i <= H; i++)
+#define drep(i, H) for (int i = H - 1; i >= 0; i--)
+#define drep1(i, H) for (int i = H; i >= 1; i--)
+#define srep(i, S, T) for (int i = S; i < T; i++)
+#define rng(a) a.begin(), a.end()
+
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using vv = vc<vc<T>>;
+template <typename T>
+using vvv = vv<vc<T>>;
+using vi = vc<int>;
+using vvi = vv<int>;
+using vvvi = vvv<int>;
+using vl = vc<long>;
+using vvl = vv<long>;
+using ll = long long;
+using vll = vc<ll>;
+using vvll = vv<ll>;
+
+int main()
+{
+	// IO高速化のおまじない
+    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	vector<pair <int,int>> d;
+
+	d.emplace_back(-1,0);
+
+	int ans = 0;
+
+	rep(i,n){
+		int a;
+		cin >> a;
+		ans++;
+
+		// first : 値
+		// second: 個数
+		if(d.back().first==a){
+			d.back().second++;
+		}else{
+			d.emplace_back(a,1);	// 初見入力
+		}
+		// 消えるかどうかの判定
+		// kと書かれたボールが、k個連続した場合に減る
+		if(d.back().first==d.back().second){
+			ans -= d.back().second;
+			d.pop_back();
+		}
+		cout << ans << endl;
+	}
+
+    return EXIT_SUCCESS;
+}
