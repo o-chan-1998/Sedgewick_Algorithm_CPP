1f83948691a1161464dcfcaf1d4164668264fb04
root <root@localhost.localdomain>
2022-06-02 09:02:07 -0400

    ABC202_C問題

---
+++ ABC202/C/sample.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ios>
+#include <iomanip>
+#include <queue>
+#include <map>
+#include <set>
+#include <climits>
+#include <bitset>
+#include <numeric> // iota関数
+#include <fstream>
+
+using namespace std;
+
+#define rep(i, H) for (int i = 0; i < H; i++)
+#define rep1(i, H) for (int i = 1; i <= H; i++)
+#define drep(i, H) for (int i = H - 1; i >= 0; i--)
+#define drep1(i, H) for (int i = H; i >= 1; i--)
+#define srep(i, S, T) for (int i = S; i < T; i++)
+#define rng(a) a.begin(), a.end()
+
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using vv = vc<vc<T>>;
+template <typename T>
+using vvv = vv<vc<T>>;
+using vi = vc<int>;
+using vvi = vv<int>;
+using vvvi = vvv<int>;
+using vl = vc<long>;
+using vvl = vv<long>;
+using ll = long long;
+using vll = vc<ll>;
+using vvll = vv<ll>;
+
+int main()
+{
+	// IO高速化のおまじない
+    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	vi a(n), b(n), c(n);
+	rep(i,n){
+		cin >> a[i];
+	}
+	rep(i,n){
+		cin >> b[i];
+	}
+	rep(i,n){
+		cin >> c[i];
+	}
+	vi cnt(n+1);
+	rep(i,n){
+		// 数列Aiの数の個数
+		cnt[a[i]]++;
+	}
+	ll ans = 0;
+	rep(j,n){
+		ans += cnt[b[c[j]-1]];
+	}
+
+	cout << ans << endl;
+
+    return EXIT_SUCCESS;
+}
