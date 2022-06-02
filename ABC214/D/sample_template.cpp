2c6c912018e4da90d0e0b86753ed2596d05f20cd
root <root@localhost.localdomain>
2022-06-02 08:51:46 -0400

    ABC214_D問題

--- ABC214/D/sample_template.cpp
+++ ABC214/D/sample_template.cpp
@@ -36,36 +36,6 @@ using ll = long long;
 using vll = vc<ll>;
 using vvll = vv<ll>;
 
-struct UnionFind {
-	vi d;
-	UnionFind(int n=0): d(n,-1) {}
-	int find(int x) {
-		if (d[x] < 0){
-			return x;
-		}
-		return d[x] = find(d[x]);
-	}
-	bool unite(int x, int y) {
-		x = find(x); 
-		y = find(y);
-		if (x == y){
-			return false;
-		}
-		if (d[x] > d[y]){
-			swap(x,y);
-		} 
-		d[x] += d[y];
-		d[y] = x;
-		return true;
-	}
-	bool same(int x, int y) { 
-		return find(x) == find(y);
-	}
-	int size(int x) { 
-		return -d[find(x)];
-	}
-};
-
 int main()
 {
 	// IO高速化のおまじない
@@ -73,33 +43,6 @@ int main()
 	cin.tie(0);
 	cout.tie(0);
 
-	int n;
-	cin >> n;
-	vector<pair<int, pair<int,int>>> es;
-
-	rep(i,n-1){
-		int u, v, w;
-		cin >> u >> v >> w;
-		u--;
-		v--;
-		es.emplace_back(w, pair<int,int>(u,v));
-	}
-
-	// 重みの昇順に整列
-	sort(es.begin(), es.end());
-
-	UnionFind d(n);
-
-	ll ans = 0;
-	
-	// 重みの小さい順にUnionFindしていく
-	for(auto p: es){
-		// 重み✕端末1のサイズ✕端末2のサイズ
-		ans += (ll)(p.first)*d.size(p.second.first)*d.size(p.second.second);
-		d.unite(p.second.first, p.second.second);
-	}
-
-	cout << ans << endl;
 
     return EXIT_SUCCESS;
 }
