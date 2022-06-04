521559cb06ea51b79bbe929efe09bfc3c14bb632
root <root@localhost.localdomain>
2022-06-04 05:50:14 -0400

    ABC243_D問題

---
+++ ABC243/D/sample.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ios>
+#include <iomanip>
+#include <queue>
+#include <map>
+#include <set>
+#include <climits>
+#include <bitset>
+#include <numeric> // iota関数
+#include <fstream>
+
+using namespace std;
+
+#define rep(i, H) for (int i = 0; i < H; i++)
+#define rep1(i, H) for (int i = 1; i <= H; i++)
+#define drep(i, H) for (int i = H - 1; i >= 0; i--)
+#define drep1(i, H) for (int i = H; i >= 1; i--)
+#define srep(i, S, T) for (int i = S; i < T; i++)
+#define rng(a) a.begin(), a.end()
+
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using vv = vc<vc<T>>;
+template <typename T>
+using vvv = vv<vc<T>>;
+using vi = vc<int>;
+using vvi = vv<int>;
+using vvvi = vvv<int>;
+using vl = vc<long>;
+using vvl = vv<long>;
+using ll = long long;
+using vll = vc<ll>;
+using vvll = vv<ll>;
+
+int main()
+{
+	// IO高速化のおまじない
+    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	ll x;
+	cin >> n >> x;
+	string s;
+	cin >> s;
+
+	string t;
+	for(char c:s){
+		if(c=='U'){
+			if(t.size() && t.back() != 'U'){
+				t.pop_back();
+			}else{
+				t += c;
+			}
+		}else{
+			t += c;
+		}
+	}
+
+	for(char c:t){
+		if(c=='U'){
+			x /= 2;
+		}
+		if(c=='L'){
+			x *= 2;
+		}
+		if(c=='R'){
+			x = 2*x+1;
+		}
+	}
+
+	cout << x << endl;
+
+    return EXIT_SUCCESS;
+}
