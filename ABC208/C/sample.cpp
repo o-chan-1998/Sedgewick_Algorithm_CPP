570fa69a656b455f3621762633c8b071187c41cc
root <root@localhost.localdomain>
2022-06-08 12:21:06 -0400

    ABC208_C問題

---
+++ ABC208/C/sample.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ios>
+#include <iomanip>
+#include <queue>
+#include <map>
+#include <set>
+#include <climits>
+#include <bitset>
+#include <numeric> // iota関数
+#include <fstream>
+
+using namespace std;
+
+#define rep(i, H) for (int i = 0; i < H; i++)
+#define rep1(i, H) for (int i = 1; i <= H; i++)
+#define drep(i, H) for (int i = H - 1; i >= 0; i--)
+#define drep1(i, H) for (int i = H; i >= 1; i--)
+#define srep(i, S, T) for (int i = S; i < T; i++)
+#define rng(a) a.begin(), a.end()
+
+template <typename T>
+using vc = vector<T>;
+template <typename T>
+using vv = vc<vc<T>>;
+template <typename T>
+using vvv = vv<vc<T>>;
+using vi = vc<int>;
+using vvi = vv<int>;
+using vvvi = vvv<int>;
+using vl = vc<long>;
+using vvl = vv<long>;
+using ll = long long;
+using vll = vc<ll>;
+using vvll = vv<ll>;
+
+int main()
+{
+	// IO高速化のおまじない
+    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+    int n;
+    ll k;
+    cin >> n >> k;
+
+    vi a(n);
+    rep(i,n){
+        cin >> a[i];
+    }
+
+    map<ll,int> sort_a;
+    rep(i,n){
+        sort_a[a[i]] = i+1;
+    }
+    sort(sort_a.begin(),sort_a.end());
+
+
+    return EXIT_SUCCESS;
+}
