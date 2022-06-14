6567fed939bb5ea34c3a25dee2eeabfbcaf5595d
root <root@localhost.localdomain>
2022-06-14 10:05:10 -0400

    ABC203_D問題

--- ABC203/D/sample.py
+++ ABC203/D/sample.py
@@ -1,36 +1,58 @@
-N, W = 6, 15
+N,K=3,2
+A=[[1,7,0],[5,8,11],[10,4,2]]
 
-dp = [[0]*(W+1) for i in range(N+1)]
+#達成できる中央値で二分探索
+ng=-1
+ok=10**9
 
-print(N, "個の商品に対して、持ち帰り可能な重さは最大", W, "である")
-print("DPテーブルの初期状態")
-for debug_data in dp:
-    print(debug_data)
-print("-"*30)
+while ok-ng!=1:
+    ima=(ng+ok)//2
+    print(ima)
+    #imaが達成できるか？
+    l=[[0]*N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if j==0 and A[i][j]<=ima:
+                l[i][j]=1
+            elif A[i][j]<=ima:
+                l[i][j]=1+l[i][j-1]
+            else:
+                l[i][j]=l[i][j-1]
+    for debug_data in l:
+        print(debug_data)
+    print("o"*30)
 
-for n, wi, vi in [[1, 6, 5], [2, 5, 6], [3, 6, 4], [4, 6, 6], [5, 3, 5], [6, 7, 2]]:
+    # 縦方向の累積和
+    for j in range(N):
+        for i in range(1,N):
+            l[i][j]+=l[i-1][j]
+    for debug_data in l:
+        print(debug_data)
+    print("_"*30)
 
-    print(n,"番目の品物")
-
-    for w in range(W+1):
-        print(w,"の重さに対してDPテーブル更新")
-    
-        if w-wi < 0:
-            print(n,"番目の商品を持ち帰れない(",wi,")ので、前のテーブルをコピーする")
-            dp[n][w] = dp[n-1][w]
+    # K*Kの区画を確認
+    for i in range(K-1,N):
+        for j in range(K-1,N):
+            print(i,j,"の座標を探索")
+            small=l[i][j]   
+            print("l[",i,"][",j,"]の値は",small)
+            if i!=K-1:
+                print(i,"!=",K-1,"となる場合")
+                small-=l[i-K][j]
+            if j!=K-1:
+                print(j,"!=",K-1,"となる場合")
+                small-=l[i][j-K]
+            if i!=K-1 and j!=K-1:
+                small+=l[i-K][j-K]
+            if small>=(K**2+1)//2:
+                ok=ima
+                break
         else:
-            print(n,"番目の商品：重さ",wi,"、価値",vi,"でテーブル更新")
-            if dp[n-1][w] > dp[n-1][w-wi]+vi:
-                dp[n][w] = dp[n-1][w]
-                for debug_data in dp[n-1:n+1]:
-                    print(debug_data)
-                input("*"*30)
-            else :
-                dp[n][w] = dp[n-1][w-wi]+vi
-                for debug_data in dp[n-1:n+1]:
-                    print(debug_data)
-                input("*"*30)
-
-for debug_data in dp:
-    print(debug_data)
-print(dp[-1][-1])
+            print("continue")
+            continue
+        break
+    else:
+        ng=ima
+        print("ng:",ng)
+    input("-"*30)
+print(ok)
